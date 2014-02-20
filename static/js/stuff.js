fb3c4dad69dd27294bea6857f087476df0d6b223
Renee Salz <rsalz2020@gmail.com>
2014-02-19 19:20:09 -0800

    Completed Hello World problem!

---
+++ static/js/stuff.js
@@ -0,0 +1,24 @@
+alert('who are you and why are you here'); // edit me!
+
+// Problem 1 (Say Hello!) ---------------------------------------------------
+$('#say_hello').click(function() {
+  alert('Hello World!')
+});
+
+
+// Problem 2 (Houdini) ------------------------------------------------------
+$('#disappear').click(function() {
+  //WRITE CODE HERE
+});
+
+$('#reappear').click(function() {
+  //WRITE CODE HERE
+});
+
+
+// Problem 3 (Tickle Me Pink) -----------------------------------------------
+// WRITE CODE HERE
+
+
+// Problem 4 (Greet Me) -----------------------------------------------------
+// WRITE CODE HERE
