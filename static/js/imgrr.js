7d835800d05724ccdeccd2bea88e03cb59ee4645
Renee Salz <rsalz2020@gmail.com>
2014-02-26 21:50:42 -0800

    Actually completed animals problem!

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,13 +1,30 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
-// WRITE CODE HERE
+ $('#toggle_img').click(function() {
+      if ($('#toggle_img').text()==='Go Away!') {
+      $('#main_img').hide()
+      $('#toggle_img').text('Come Back!')
+  } else {
+  	$('#main_img').show()
+    $('toggle_img').text('Go Away!')
+}
+
+})
+
+  
+
 
 // Problem 3 (Swap Em) -----------------------------------------------
-// WRITE CODE HERE
+$('#change_img').click(function() {
+	$('#main_img').attr('src','/static/img/'+ $('#new_img_file').val())
+
+})
 
 // Problem 4 (Find the Source) -------------------------------------------------
 $('.clickable').click(function() {
-  // WRITE CODE HERE
+  alert($(this).attr('src'))
 });
 
 // Problem 5 (Imgrr) -------------------------------------------------
-// WRITE CODE HERE
+$('.clickable').click(function() {
+  $('#second_img').attr('src', $(this).attr('src'))
+});
