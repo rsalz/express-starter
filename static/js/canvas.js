aa9ce4cdc260f64ac88e667eb125f624a4512522
Renee Salz <rsalz2020@gmail.com>
2014-03-05 19:03:54 -0800

    Drew all my shapes

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -13,8 +13,35 @@ $(document).ready(function() {
   })
 
   $('#p2').click(function() {
-  	context.strokeRect(150,10,100,100)
+  	context.strokeRect(150,10,100,100);
   })
+
+  $('#p3').click(function() {
+  	context.beginPath();
+  		context.arc(60,290,50,Math.PI,0);
+  	context.closePath();
+  	context.stroke();
+  	
+  })
+
+    $('#p4').click(function() {
+  	context.beginPath();
+  		context.arc(200,190,50,0,Math.PI);
+  		context.arc(200,190,50,Math.PI,0);
+  	context.closePath();
+  	context.stroke();
+  	
+  })
+
+   $('#p5').click(function() {
+  	context.beginPath();
+  		context.moveTo(300,10);
+  		context.lineTo(300,200);
+  	context.closePath();
+  	context.stroke();
+
+  })
+
   //Write your code for p1-p12 here
   //
 
